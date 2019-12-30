8e143aa8183c7b92e96782f61da0b7849cb0dfe4
atefeh-syf <atefeh.syf@gmail.com>
2019-12-30 22:00:48 +0330

    Fourth commit

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -5,57 +5,26 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="form-group row mb-0 justify-content-center ">
-                <button type="button" class="btn btn-insertmem center-block">
-                    {{ __('+ ثبت خاطره جدید ') }}
-                </button>
+                <a type="button" class="btn btn-insertmem center-block">{{ __('+ ثبت خاطره جدید ') }} </a>
             </div>
         </div>
     </div> 
-    <div class="row memry-container justify-content-center">   
-        <div class="col-md-8">
-            <div class="row justify-content-center">
-                @foreach ($user->memories as $memory)
-                    <div class="col-md-6  pt-4">
-                        <div class="card">
-                            <div class="row justify-content-center ">
-                                <div class="col-md-6"><div class="card-header">98/10/4</div></div>
-                                <div class="col-md-6"><div class="card-header">{{ $memory->title }}</div></div>
-                            </div>
-                            <div class="row justify-content-center">
-                                    <button type="submit" class="btn btn-del center-block">
-                                        {{ __(' حذف ') }}
-                                    </button>
-                                    <button type="submit" class="btn btn-edit center-block">
-                                        {{ __(' ویرایش ') }}
-                                    </button>
-                                    <button type="submit" class="btn btn-reg center-block">
-                                        {{ __(' مشاهده ') }}
-                                    </button>
-                            </div>
-                        </div>
+    <div class="row memry-container pt-4 justify-content-center ">  
+    @foreach ($user->memories as $memory) 
+        <div class="col-5 pt-4">
+                <div class="card">
+                    <div class="row justify-content-center ">
+                        <div class="col-md-6"><div class="card-header">98/10/4</div></div>
+                        <div class="col-md-6"><div class="card-header">{{ $memory->title }}</div></div>
                     </div>
-                @endforeach
-                
-            </div>
-        </div>
-    </div>
-
-
-
-            <!--<div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="row justify-content-center">
+                            <a type="submit" class="btn btn-del center-block"> {{ __(' حذف ') }} </a>
+                            <a type="submit" class="btn btn-edit center-block">{{ __(' ویرایش ') }}</a>
+                            <a type="submit" class="btn btn-reg center-block"> {{ __(' مشاهده ') }} </a>
                         </div>
-                    @endif
-
-                    You are logged in!
                 </div>
-            </div>-->
-        
-    
+         </div>
+    @endforeach
+    </div>  
 </div>
 @endsection
