a7f3bb2fb2cfa23a957aa9a55bf1b12216387675
atefeh-syf <atefeh.syf@gmail.com>
2020-01-02 23:07:13 +0330

    final commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,4 +72,8 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+    public function username()
+    {
+        return 'username';
+    }
 }
